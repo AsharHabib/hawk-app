2d13ba2b063053bd2c760ed1690c8396dc3940b2
Raymond Amparo <ray.amparo1995@gmail.com>
2021-07-23 18:34:11 -0700

    test

--- hars-project-2-dev/hawk-app/src/main/java/driver/Main.java
+++ hars-project-2-dev/hawk-app/src/main/java/driver/Main.java
@@ -11,6 +11,19 @@ public class Main {
 			config.addStaticFiles("/public");
 		}).start(5000);
 
+		app.get("/airport-results", context -> {
+	    	context.render("./public/airport_results.html");
+	    });
+	    
+	    app.get("/search-results", context -> {
+	    	context.render("./public/search_results.html");
+	    });
+	    
+	    app.get("/price-results", context -> {
+	    	context.render("./public/price_results.html");
+	    });
+
+		
 		FrontController fc = new FrontController(app);
 	}
 }
