beddfff97972899f640c51a0189919d76b9290c9
Raymond Amparo <ray.amparo1995@gmail.com>
2021-07-24 20:41:59 -0700

    added updated pages

--- hars-project-2-dev/hawk-app/src/main/java/driver/Main.java
+++ hars-project-2-dev/hawk-app/src/main/java/driver/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		
 		Javalin app = Javalin.create(config -> {
 			config.addStaticFiles("/public");
-		}).start(5000);
+		}).start(5001);
 
 		app.get("/airport-results", context -> {
 	    	context.render("./public/airport_results.html");
